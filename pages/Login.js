e615e732b58bbe875ccc00b713ff5791391d7cc8
jemerardprete <julie.emerard@sfr.fr>
2022-12-15 11:50:07 +0100

    style: 💄 create login and register forms

--- pages/Login.js
+++ pages/Login.js
@@ -1,9 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { Form, FormItem } from 'react-native-form-component';
+
 
 export default function Login() {
     return (
-        <View>
-            <Text>Formulaire de connexion</Text>
-        </View>
+        <View style={styles.container}>
+            <Text style={styles.pageTitle}>Connexion</Text>
+            <Form onButtonPress={() => console.warn('do something')} buttonText="Se connecter" buttonStyle={styles.button}>
+                <FormItem
+                    label="Email"
+                    isRequired
+                    asterik
+                    style={styles.input}
+                    keyboardType="email-address"
+                />
+                <FormItem
+                    label="Mot de passe"
+                    isRequired
+                    asterik
+                    secureTextEntry={true}
+                    style={styles.input}
+                />
+            </Form>
+        </View >
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        paddingHorizontal: 10,
+        paddingVertical: 50
+    },
+    button: {
+        backgroundColor: '#2596be'
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: 'lightgrey'
+    },
+    pageTitle: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 20,
+        marginBottom: 30
+    }
+});
--- pages/Register.js
+++ pages/Register.js
@@ -1,9 +1,54 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { Form, FormItem } from 'react-native-form-component';
+
 
 export default function Register() {
     return (
-        <View>
-            <Text>Formulaire d'inscription</Text>
-        </View>
+        <View style={styles.container}>
+            <Text style={styles.pageTitle}>Inscription</Text>
+            <Form onButtonPress={() => console.warn('do something')} buttonText="Se connecter" buttonStyle={styles.button}>
+                <FormItem
+                    label="Nom d'utilisateur"
+                    isRequired
+                    asterik
+                    style={styles.input}
+                />
+                <FormItem
+                    label="Email"
+                    isRequired
+                    asterik
+                    style={styles.input}
+                    keyboardType="email-address"
+                />
+                <FormItem
+                    label="Mot de passe"
+                    isRequired
+                    asterik
+                    secureTextEntry={true}
+                    style={styles.input}
+                />
+            </Form>
+        </View >
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        paddingHorizontal: 10,
+        paddingVertical: 50
+    },
+    button: {
+        backgroundColor: '#2596be'
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: 'lightgrey'
+    },
+
+    pageTitle: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 20,
+        marginBottom: 30
+    }
+});
